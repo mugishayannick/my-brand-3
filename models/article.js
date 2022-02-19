d98b9b623d2b5c6b2eea269e22b7c7c5421902ff
Yannick <yannickmugisha23@gmail.com>
2022-02-19 18:08:13 +0200

    remove require

--- models/article.js
+++ models/article.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 
 
 const articleSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    userId: { type: String, required: true },
+    title: { type: String},
+    content: { type: String},
+    imageUrl: { type: String },
+    userId: { type: String},
 
 });
 
