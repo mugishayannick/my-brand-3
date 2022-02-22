35d2c67b35ba4589de0fc85c8f2048765abcbe8f
Yannick <yannickmugisha23@gmail.com>
2022-02-22 15:42:40 +0200

    image url

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+// import  mongoose  from "mongoose";
+
+// const commentSchema = new mongoose.commentSchema({
+//     author: String,
+//     comment: String
+
+// })
+// const Comment = mongoose.model('Comment', commentSchema);
+
+// export default Comment;
---
+++ routes/comment.js
@@ -0,0 +1,14 @@
+// import express from 'express'
+// import { authMiddleware } from "../middleware/auth.js";
+// import {getComment} from "../controllers/comment.js"
+// import router from './message';
+
+
+
+// const router = express(router);
+
+
+// router.post('/api/post/comment/:id', authMiddleware, getComment);
+
+
+// export default router;
